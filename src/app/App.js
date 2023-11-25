81afe99483efbf7480007b03259a06c21e6c4899
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-11-24 22:13:12 -0300

    style(layout): make layout of the app

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Quote from "../components/Quote";
+import './App.css';
+
 function App() {
   return (
     <div className="App">
+      <Header />
+      <main className="flex-1">
+        <Quote id='1' />
+      </main>
+      <Footer />
     </div>
   );
 }
